a62c21fc9b08c0cd4cd726cbb2e0442490d9a317
erikaCutugno <erikucciapop@gmail.com>
2025-01-28 20:44:17 +0100

    feat: bonus

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,14 @@
 import Header from "./Components/Header";
 import Main from "./Components/Main";
 import Footer from "./Components/Footer";
+import FooterTop from "./Components/FooterTop";
 
 export default function App() {
   return (
     <>
       <Header />
       <Main />
+      <FooterTop />
       <Footer />
     </>
   );
--- src/Components/Footer.jsx
+++ src/Components/Footer.jsx
@@ -1,36 +1,6 @@
 export default function Footer() {
   return (
     <footer>
-      <div className="footer-top">
-        <div className="container">
-          <div className="card">
-            <img
-              src="./img/buy-comics-digital-comics.png"
-              alt="Digital comics"
-            />
-            <h3>digital comics</h3>
-          </div>
-          <div className="card">
-            <img src="./img/buy-comics-merchandise.png" alt="Dc merchandise" />
-            <h3>Dc merchandise</h3>
-          </div>
-          <div className="card">
-            <img src="./img/buy-comics-subscriptions.png" alt="subscriptions" />
-            <h3>subscriptions</h3>
-          </div>
-          <div className="card">
-            <img
-              src="./img/buy-comics-shop-locator.png"
-              alt="comic shop locator"
-            />
-            <h3>comic shop locator</h3>
-          </div>
-          <div className="card">
-            <img src="./img/buy-dc-power-visa.svg" alt="Dc power visa" />
-            <h3 className="power">dc power visa</h3>
-          </div>
-        </div>
-      </div>
       <div className="footer-center">
         <div className="container">
           <div className="row">
