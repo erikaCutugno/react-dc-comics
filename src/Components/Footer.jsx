2ec0a188043611ec8b4aee9111ab87cd9a0223d1
erikaCutugno <erikucciapop@gmail.com>
2025-01-28 18:13:40 +0100

    feat: add footer-center

--- src/Components/Footer.jsx
+++ src/Components/Footer.jsx
@@ -31,7 +31,114 @@ export default function Footer() {
           </div>
         </div>
       </div>
-      <div className="footer-center"></div>
+      <div className="footer-center">
+        <div className="container">
+          <div className="row">
+            <div className="col">
+              <h3>Dc comics</h3>
+              <nav className="footer-nav">
+                <ul>
+                  <li>
+                    <a href="#">characters</a>
+                  </li>
+                  <li>
+                    <a href="#">comics</a>
+                  </li>
+                  <li>
+                    <a href="#">movies</a>
+                  </li>
+                  <li>
+                    <a href="#">tv</a>
+                  </li>
+                  <li>
+                    <a href="#">games</a>
+                  </li>
+                  <li>
+                    <a href="#">videos</a>
+                  </li>
+                  <li>
+                    <a href="#">news</a>
+                  </li>
+                </ul>
+              </nav>
+              <h3>shop</h3>
+              <nav className="footer-nav">
+                <ul>
+                  <li>
+                    <a href="#">Shop DC</a>
+                  </li>
+                  <li>
+                    <a href="#">Shop DC Collectibles</a>
+                  </li>
+                </ul>
+              </nav>
+            </div>
+            <div className="col">
+              <h3>Dc</h3>
+              <nav className="footer-nav">
+                <ul>
+                  <li>
+                    <a href="#">terms of use</a>
+                  </li>
+                  <li>
+                    <a href="#">privacy policy (new)</a>
+                  </li>
+                  <li>
+                    <a href="#">ad choices</a>
+                  </li>
+                  <li>
+                    <a href="#">advertising</a>
+                  </li>
+                  <li>
+                    <a href="#">jobs</a>
+                  </li>
+                  <li>
+                    <a href="#">subscriptions</a>
+                  </li>
+                  <li>
+                    <a href="#">talent workshops</a>
+                  </li>
+                  <li>
+                    <a href="#">CPSC certificates</a>
+                  </li>
+                  <li>
+                    <a href="#">ratings</a>
+                  </li>
+                  <li>
+                    <a href="#">shop help</a>
+                  </li>
+                  <li>
+                    <a href="#">contact us</a>
+                  </li>
+                </ul>
+              </nav>
+            </div>
+            <div className="col">
+              <h3>sites</h3>
+              <nav className="footer-nav">
+                <ul>
+                  <li>
+                    <a href="#">DC</a>
+                  </li>{" "}
+                  <li>
+                    <a href="#">MAD magazine</a>
+                  </li>{" "}
+                  <li>
+                    <a href="#">DC kids</a>
+                  </li>{" "}
+                  <li>
+                    <a href="#">DC universe</a>
+                  </li>{" "}
+                  <li>
+                    <a href="#">DC power visa</a>
+                  </li>
+                </ul>
+              </nav>
+            </div>
+          </div>
+          <div className="logo-big"></div>
+        </div>
+      </div>
       <div className="footer-bottom"></div>
     </footer>
   );
