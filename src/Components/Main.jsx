fa895fabcc8c4ad3fea62d7e77a8a6f7ab45f0cf
erikaCutugno <erikucciapop@gmail.com>
2025-01-28 16:05:54 +0100

    feat: add main

--- src/Components/Main.jsx
+++ src/Components/Main.jsx
@@ -1,3 +1,9 @@
 export default function Main() {
-  return <h1>Main</h1>;
+  return (
+    <main>
+      <div className="container">
+        <h1>--&gt; Content goes here &lt;--</h1>
+      </div>
+    </main>
+  );
 }
