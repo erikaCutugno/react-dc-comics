caac5e9ad373a931b84dab7e13521b47aa72f8f9
erikaCutugno <erikucciapop@gmail.com>
2025-01-28 15:57:52 +0100

    feat: add header

---
+++ src/Components/Header.jsx
@@ -0,0 +1,41 @@
+export default function Header() {
+  return (
+    <header className="container">
+      <img src="./img/dc-logo.png" alt="Logo" />
+      <nav className="header-nav">
+        <ul>
+          <li>
+            <a href="#">characters</a>
+          </li>
+          <li className="active">
+            <a href="#">comics</a>
+          </li>
+          <li>
+            <a href="#">movies</a>
+          </li>
+          <li>
+            <a href="#">tv</a>
+          </li>
+          <li>
+            <a href="#">games</a>
+          </li>
+          <li>
+            <a href="#">collectibles</a>
+          </li>
+          <li>
+            <a href="#">videos</a>
+          </li>
+          <li>
+            <a href="#">fans</a>
+          </li>
+          <li>
+            <a href="#">news</a>
+          </li>
+          <li>
+            <a href="#">shop</a>
+          </li>
+        </ul>
+      </nav>
+    </header>
+  );
+}
