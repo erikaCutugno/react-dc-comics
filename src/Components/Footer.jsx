6245e7f6b4e7bfb56f6a3b50badd168ca9f6328d
erikaCutugno <erikucciapop@gmail.com>
2025-01-28 20:36:04 +0100

    feat: add footer-bottom

--- src/Components/Footer.jsx
+++ src/Components/Footer.jsx
@@ -139,7 +139,43 @@ export default function Footer() {
           <div className="logo-big"></div>
         </div>
       </div>
-      <div className="footer-bottom"></div>
+      <div className="footer-bottom">
+        <div className="container">
+          <button className="btn">sign-up now!</button>
+          <div className="social">
+            <h4>Follow us</h4>
+            <nav className="footer-nav">
+              <ul>
+                <li>
+                  <a href="#">
+                    <img src="./img/footer-facebook.png" alt="facebook" />
+                  </a>
+                </li>
+                <li>
+                  <a href="#">
+                    <img src="./img/footer-twitter.png" alt="twitter" />
+                  </a>
+                </li>
+                <li>
+                  <a href="#">
+                    <img src="./img/footer-youtube.png" alt="youtube" />
+                  </a>
+                </li>
+                <li>
+                  <a href="#">
+                    <img src="./img/footer-pinterest.png" alt="pinterest" />
+                  </a>
+                </li>
+                <li>
+                  <a href="#">
+                    <img src="./img/footer-periscope.png" alt="periscope" />
+                  </a>
+                </li>
+              </ul>
+            </nav>
+          </div>
+        </div>
+      </div>
     </footer>
   );
 }
