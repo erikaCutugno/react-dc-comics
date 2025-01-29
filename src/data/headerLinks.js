f5bf4782339c1370c8ed86528a1a6d5620b3357c
erikaCutugno <erikucciapop@gmail.com>
2025-01-29 15:15:49 +0100

    feat: add array link header

---
+++ src/data/headerLinks.js
@@ -0,0 +1,62 @@
+export default [
+  {
+    id: 1,
+    url: "#",
+    text: "characters",
+    current: false,
+  },
+  {
+    id: 2,
+    url: "#",
+    text: "comics",
+    current: true,
+  },
+  {
+    id: 3,
+    url: "#",
+    text: "movies",
+    current: false,
+  },
+  {
+    id: 4,
+    url: "#",
+    text: "tv",
+    current: false,
+  },
+  {
+    id: 5,
+    url: "#",
+    text: "games",
+    current: false,
+  },
+  {
+    id: 6,
+    url: "#",
+    text: "collectibles",
+    current: false,
+  },
+  {
+    id: 7,
+    url: "#",
+    text: "videos",
+    current: false,
+  },
+  {
+    id: 8,
+    url: "#",
+    text: "fans",
+    current: false,
+  },
+  {
+    id: 9,
+    url: "#",
+    text: "news",
+    current: false,
+  },
+  {
+    id: 10,
+    url: "#",
+    text: "shop",
+    current: false,
+  },
+];
